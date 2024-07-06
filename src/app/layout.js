1d3b7453067fbdb2adc511c061cc41a67a32c597
Ehsan <ctech1699@gmail.com>
2024-07-07 00:21:09 +0530

    top loader

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar/Sidebar/Sidebar";
 import Navbar from "@/components/Navbar/Navbar";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#151c2c] text-white`}>
+      <NextTopLoader />
         <div className="flex h-screen">
+
           <aside className="w-1/4 bg-[#182237] text-white p-4 m-4">
             <Sidebar/>
           </aside>
